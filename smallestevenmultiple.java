d9f939e2a1344882835acd6645708f6de7c091ca
varnicagarg25 <varnicagarg07@gmail.com>
2026-07-29 21:47:03 +0530

    Implement method to find smallest even multiple

---
+++ smallestevenmultiple.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int smallestEvenMultiple(int n) {
+        if(n%2==0){
+            System.out.println(n);
+            return n;
+        }
+        else{
+            System.out.println(n*2);
+            return n*2;
+        }
+        
+    }
+}
