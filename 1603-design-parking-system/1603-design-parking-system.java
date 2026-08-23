ab9ddc5e108fda3350835f115f7e481492de853c
varnicagarg25 <varnicagarg07@gmail.com>
2026-08-23 21:43:28 +0530

    Time: 8 ms (60.4%), Space: 46.8 MB (92.33%) - LeetHub

---
+++ 1603-design-parking-system/1603-design-parking-system.java
@@ -0,0 +1,17 @@
+class ParkingSystem {
+    private int[] count;
+
+    public ParkingSystem(int big, int medium, int small) {
+        // Index 1: big, Index 2: medium, Index 3: small
+        count = new int[]{0, big, medium, small};
+    }
+    
+    public boolean addCar(int carType) {
+        if (count[carType] > 0) {
+            count[carType]--;
+            return true;
+        }
+        return false;
+    }
+}
+
