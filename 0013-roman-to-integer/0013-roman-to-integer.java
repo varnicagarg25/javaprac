ff40765d3af72cd53451dfdfec016c4ba7786fa2
varnicagarg25 <varnicagarg07@gmail.com>
2026-08-30 11:37:02 +0530

    Time: 2 ms (100%), Space: 46 MB (98.32%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int romanToInt(String s) {
+        int total = 0;
+        int prevValue = 0;
+
+        for (int i = s.length() - 1; i >= 0; i--) {
+            int currValue = getValue(s.charAt(i));
+
+            if (currValue < prevValue) {
+                total -= currValue;
+            } else {
+                total += currValue;
+            }
+
+            prevValue = currValue;
+        }
+
+        return total;
+    }
+
+    private int getValue(char ch) {
+        switch (ch) {
+            case 'I': return 1;
+            case 'V': return 5;
+            case 'X': return 10;
+            case 'L': return 50;
+            case 'C': return 100;
+            case 'D': return 500;
+            case 'M': return 1000;
+            default: return 0;
+        }
+    }
+}
