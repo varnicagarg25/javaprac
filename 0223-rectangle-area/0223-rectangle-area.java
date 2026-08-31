93f59994ae938d3bc7235f64b2a1c4af614a78f9
varnicagarg25 <varnicagarg07@gmail.com>
2026-08-31 15:23:31 +0530

    Time: 2 ms (67.09%), Space: 44.3 MB (67.44%) - LeetHub

--- 0223-rectangle-area/0223-rectangle-area.java
+++ 0223-rectangle-area/0223-rectangle-area.java
@@ -1,14 +1,13 @@
 class Solution {
     public int computeArea(int ax1, int ay1, int ax2, int ay2, int bx1, int by1, int bx2, int by2) {
 
-        int areaA = (ax2 - ax1) * (ay2 - ay1);
-        int areaB = (bx2 - bx1) * (by2 - by1);
-
-        int overlapWidth = Math.max(0, Math.min(ax2, bx2) - Math.max(ax1, bx1));
-        int overlapHeight = Math.max(0, Math.min(ay2, by2) - Math.max(ay1, by1));
-
-        int overlapArea = overlapWidth * overlapHeight;
-
-        return areaA + areaB - overlapArea;
+      int area1=(ax2-ax1)*(ay2-ay1);
+      int area2=(bx2-bx1)*(by2-by1);
+      int h1=Math.max(0,Math.min(ay2,by2)-Math.max(by1,ay1));
+      int w1=Math.max(0,Math.min(ax2,bx2)-Math.max(ax1,bx1));
+      int areasum=area1+area2;
+      int overarea=h1*w1;
+      int areat=areasum-overarea;
+      return areat;
     }
 }
